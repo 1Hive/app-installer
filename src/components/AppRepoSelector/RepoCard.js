d3caeb1c0712601dbfede915dac0020f48e7a4a5
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-04-28 10:51:44 -0300

    Imrpove repo card UI

--- src/components/AppRepoSelector/RepoCard.js
+++ src/components/AppRepoSelector/RepoCard.js
@@ -15,7 +15,7 @@ function RepoCard({ repo, onRepoSelected, selected = true }) {
         box-shadow: ${selected ? `0px 1px 2px 0px rgba(0, 0, 0, 0.2)` : ''};
         border: 1px solid ${selected ? 'transparent' : theme.border.alpha(0.6)};
         border-radius: 6px;
-        padding: ${3 * GU}px;
+        padding: ${3 * GU}px ${4 * GU}px;
         transition: all 0.4s ease;
         cursor: pointer;
 
@@ -30,29 +30,16 @@ function RepoCard({ repo, onRepoSelected, selected = true }) {
     >
       <div
         css={`
+          min-height: ${10 * GU}px;
           display: flex;
-          align-items: center;
         `}
       >
         <div
           css={`
-            width: ${7 * GU}px;
-          `}
-        >
-          <div
-            css={`
-              display: flex;
-              border-radius: ${1 * GU}px;
-              margin-bottom: ${1 * GU}px;
-              overflow: hidden;
-            `}
-          >
-            <img src={repo.iconSrc} alt="" height={7 * GU} />
-          </div>
-        </div>
-        <div
-          css={`
-            margin: 0 ${3 * GU}px;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
           `}
         >
           <div
@@ -61,35 +48,63 @@ function RepoCard({ repo, onRepoSelected, selected = true }) {
               align-items: center;
             `}
           >
-            <h2
+            <div
               css={`
-                ${textStyle('title4')};
-                margin-right: ${0.5 * GU}px;
-                margin-bottom: ${0.5 * GU}px;
+                width: ${7 * GU}px;
               `}
             >
-              {repo.name}
-            </h2>
-            {repo.author && (
-              <span
+              <div
+                css={`
+                  display: flex;
+                  border-radius: ${1 * GU}px;
+                  overflow: hidden;
+                `}
+              >
+                <img src={repo.iconSrc} alt="" height={7 * GU} />
+              </div>
+            </div>
+            <div
+              css={`
+                margin: 0 ${3 * GU}px;
+              `}
+            >
+              <div
+                css={`
+                  display: flex;
+                  align-items: center;
+                `}
+              >
+                <h2
+                  css={`
+                    ${textStyle('title4')};
+                    margin-right: ${0.5 * GU}px;
+                    margin-bottom: ${0.5 * GU}px;
+                  `}
+                >
+                  {repo.name}
+                </h2>
+                {repo.author && (
+                  <span
+                    css={`
+                      color: ${theme.contentSecondary};
+                    `}
+                  >
+                    By <Tag>{repo.author}</Tag>
+                  </span>
+                )}
+              </div>
+              <p
                 css={`
                   color: ${theme.contentSecondary};
                 `}
               >
-                By <Tag>{repo.author}</Tag>
-              </span>
-            )}
+                {repo.description}
+              </p>
+            </div>
           </div>
-          <p
-            css={`
-              color: ${theme.contentSecondary};
-            `}
-          >
-            {repo.description}
-          </p>
-        </div>
-        <div>
-          <Checkbox checked={selected} />
+          <div>
+            <Checkbox checked={selected} />
+          </div>
         </div>
       </div>
     </div>
